8eeb7f29df19ed479bbad6d9cdab5d754d50f58a
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-04-20 10:29:58 -0600

    Week5 Assesment

--- LaunchMod1Week5Assessment/User.cs
+++ LaunchMod1Week5Assessment/User.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace User
+namespace UserNameSpace
 {
     public class User
     {
         public string Name { get; private set; }
         public string Email { get; private set; }
-        private string Password { get; set; }
+        private string Password {get; set; }
         public bool IsLoggedIn { get; set; }
 
+        public bool PasswordCreated;
         public User(string name, string email)
         {
             Name = name;
@@ -41,7 +42,8 @@ namespace User
 
             if (Password != null)
             {
-                var confirmation = "Password Created";
+                PasswordCreated = true;
+                //I had no idea how to access the confirmation variable or Password property due to it being private so my last minute work around was to create a new bool value to confirm if a password was created.
             }
         }
 
--- UserTests/UserTest.cs
+++ UserTests/UserTest.cs
@@ -1,3 +1,4 @@
+using UserNameSpace;
 namespace UserTests
 {
     public class UserTest
@@ -5,7 +6,48 @@ namespace UserTests
         [Fact]
         public void IsCreated()
         {
+            User user1 = new User("Eli", "123@gmail.com");
 
+            Assert.Equal("Eli", user1.Name);
+            Assert.Equal("123@gmail.com", user1.Email);
+
+        }
+
+        [Fact]
+        public void Check_IsSetupCompleteMethod_ReturnsTrueBool()
+        {
+            User user1 = new User("Eli", "123@gmail.com");
+            
+            user1.CreatePassword("123@gmail.com", "abc");
+            bool setup = user1.IsSetupComplete();
+
+            Assert.True(setup);
+
+        }
+
+        [Fact]
+        public void Check_CreatePasswordMethod_AssignsAPasswordToPasswordProperty()
+        {
+            User user1 = new User("Eli", "123@gmail.com");
+            user1.CreatePassword("123@gmail.com", "abc");
+
+            Assert.True(user1.PasswordCreated);
+        }
+
+        [Fact]
+        public void Check_LogInMethod_ReturnsAStringBasedOnIfPasswordsMatch()
+        {
+            User user1 = new User("Eli", "123@gmail.com");
+            user1.CreatePassword("123@gmail.com", "abc");
+
+            User user2 = new User("Eli2", "1234@gmail.com");
+            user2.CreatePassword("abc@gmail.com", "123");
+
+            string loggedIn = user1.LogIn("abc");
+            string notLoggedIn = user2.LogIn("efg");
+
+            Assert.Equal("Logged In", loggedIn);
+            Assert.Equal("Not Logged In", notLoggedIn);
         }
     }
 }
